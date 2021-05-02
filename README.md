dd931046949bccf9500f1ea94281dce27b9513d4
redsuperbat <max.netterberg@gmail.com>
2021-05-02 22:50:10 +0200

    updated readme Dissalow mobile users Added overflow scrolling Changed position of add item button

--- README.md
+++ README.md
@@ -1,27 +1,16 @@
 # ChartMaker
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.9.
+Tool for creating basic charts in your browser. Powered by [Chart.js](https://www.chartjs.org/) and [Angular 11.2](https://angular.io/).
 
-## Development server
+## Features
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+Supports 6 different chart-types.
 
-## Code scaffolding
+- Bar
+- Pie
+- Line
+- Doughnut
+- Radar
+- Polar Area
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+Download charts as PNG files without any login directly in your browser.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { initalToolbarDesign } from 'src/assets/constants';
-import { base64UrlDecode } from 'src/utils';
-import { ToolbarService } from './services/toolbar.service';
-import { ToolbarContent, ToolbarDesign } from './types';
 
 @Component({
   selector: 'app-root',
@@ -11,42 +6,10 @@ import { ToolbarContent, ToolbarDesign } from './types';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(
-    private route: ActivatedRoute,
-    private toolbarService: ToolbarService
-  ) {}
+  constructor() {}
+  public showApp: boolean = true;
 
-  ngOnInit() {
-    // this.generateInitalGraphData();
-    // const customData = this.loadCustomData()
-    // const initalData = customData ? customData :
-    // const customToolbarContent = this.loadCustomData();
-    // const initData = customToolbarContent
-    //   ? customToolbarContent
-    //   : initalToolbarContent;
-    // this.generateInitalToolbarContent(initData);
-    // this.generateForm(initData);
+  ngOnInit(): void {
+    this.showApp = window.innerWidth > 1100;
   }
-
-  // private generateInitalGraphData(toolbarContent:ToolbarContent, toolbarDesign:ToolbarDesign) {
-  //   if (this.route.snapshot.queryParamMap.has('data')) {
-  //     const data = this.route.snapshot.queryParamMap.get('data');
-  //     if (data) {
-  //       const customData = base64UrlDecode(data).split(':::')[1];
-  //       return this.toolbarService.toolbarDesign$.next(JSON.parse(customData));
-  //     }
-  //   }
-  //   this.toolbarService.toolbarDesign$.next(initalToolbarDesign);
-  // }
-
-  // private loadCustomData() {
-  //   const data = this.route.snapshot.queryParamMap.get('data');
-  //   if (data) {
-  //     const customData: ToolbarContent = JSON.parse(
-  //       base64UrlDecode(data).split(':::')[0]
-  //     );
-  //     return customData;
-  //   }
-  //   return null;
-  // }
 }
